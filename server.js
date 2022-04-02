9554a2922671014508addf95da49c9c8451931fa
Daniel Macedo <49345274+pdmacedo@users.noreply.github.com>
2022-04-02 11:06:03 +0100

    desafio clase 7 y 8

--- server.js
+++ server.js
@@ -1,25 +1,53 @@
 const express = require("express");
-const Contenedor = require("./Contenedor.js");
-const contenedor = new Contenedor("./productos.txt");
+const { Router } = express;
+const ProductosApi = require('./api/productos.js');
 
+//ROUTER
+
+const productosApi = new ProductosApi();
+const productosRouter = new Router();
+
+//SERVIDOR
 const app = express();
+app.use(express.static('public'));
+productosRouter.use(express.json());
+productosRouter.use(express.urlencoded({extended: true}));
+app.use('/api/productos', productosRouter);
 
-app.get("/", (req, res) => {
-  res.send({ mensaje: "Desafio 3" });
-});
+//LLAMAR A LOS MÉTODOS CON LAS RUTAS
 
-app.get("/productos", async (req, res) => {
-  const prods = await contenedor.getAll();
-  res.send(prods);
-});
+app.get('/api/productos', async (req,res) => {
+    const respuesta = await productosApi.getAll();
+    res.send(respuesta);
+})
 
-app.get("/productoRandom", async (req, res) => {
-  const aleatorio = await contenedor.getById(2);
-  res.send(aleatorio);
-});
+app.get('/api/productos/:id', async (req,res) => { 
+    const id = req.params.id;
+    const respuesta= await productosApi.getById(id);
+    res.send(respuesta);
+})
 
-const server = app.listen(8080, () => {
-  console.log("Desafio 3:");
-});
+app.post('/api/productos', async (req,res) => {
+    const respuesta = await productosApi.save(req.body);
+    res.send(respuesta);
+})
 
-server.on("error", (error) => console.log(`Error en el servidor ${error}`));
+app.put('/api/productos/:id', async (req,res) => {
+    const id = req.params.id;
+    const productActualizado = req.body;
+    const respuesta = await productosApi.actualizaId(id, productActualizado);
+    res.send(respuesta);
+})
+
+app.delete('/api/productos/:id', async (req,res) => { 
+    const id = req.params.id;
+    const respuesta= await productosApi.deleteById(id);
+    res.send(respuesta);
+})
+
+const PORT = 8080;
+const server = app.listen(PORT, () => {
+    console.log(`Servidor http escuchando ${server.address().port}`);
+  });
+  
+  server.on("error", (error) => console.log(`Error en el servidor ${error}`));
