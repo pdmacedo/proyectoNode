d056f40a661aa12b6559e1101ad155e7369ccf63
Daniel Macedo <49345274+pdmacedo@users.noreply.github.com>
2022-04-26 22:18:25 +0100

    1ra entrega por terminar.

---
+++ primeraEntrega/src/main.js
@@ -0,0 +1,8 @@
+// const app = require("./server.js");
+
+// const PORT = 8080;
+// const server = app.listen(PORT, () => {
+//     console.log(`Servidor en el puerto ${server.adress().port}`);
+// })
+
+// server.on('error', error => console.log(`Error en el servidor ${error}`));
