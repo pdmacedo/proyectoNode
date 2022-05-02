f495d7c4fe2ab0f02d67e32935eb6891eb2e74b6
Daniel Macedo <49345274+pdmacedo@users.noreply.github.com>
2022-05-02 20:16:47 +0100

    Falta hacer funcionar el delete id carrito/id producto

---
+++ Primera_Entrega/server.js
@@ -0,0 +1,96 @@
+const express = require('express')
+const { Router } = express
+
+const ContenedorArchivo = require('./src/ContenedorArchivo')
+
+const productosAPi = new ContenedorArchivo('./dbProductos.json');
+const carritosAPi = new ContenedorArchivo('./dbCarritos.json');
+
+// Instancio servidor y persistencia
+
+const app = express()
+
+// Administrador
+
+const admin = true;
+
+function errorNoAdmin(ruta, metodo) {
+    const error = { error: -1,}
+    if(ruta && metodo){
+        error.descripcion = `Ruta: '${ruta}'. Metodo: ${metodo} no autorizado`;
+    } else{
+        error.descripcion = 'No autorizado';
+    }
+    return error;
+}
+
+function soloAdmins(req, res, next) {
+    if(!admin){
+        res.json(errorNoAdmin());
+    } else{
+        next();
+    }
+}
+
+// Configuro router de productos
+
+const productosRouter = new Router()
+
+productosRouter.get('/:id?', async (req, res) =>{
+    if(req.params.id){
+        res.json( await productosAPi.listar(req.params.id))
+    }else{
+        res.json(await productosAPi.listarAll())
+    }
+});
+
+productosRouter.post('/', soloAdmins, async (req, res) =>{
+    res.json(await productosAPi.guardar(req.body))
+})
+
+productosRouter.put('/:id', soloAdmins, async (req, res) =>{
+    res.json(await productosAPi.actualizar(req.body, req.params.id))
+})
+
+productosRouter.delete('/:id', soloAdmins, async (req, res) =>{
+    res.json(await productosAPi.borrar(req.params.id))
+})
+
+//--------------------------------------------
+// configuro router de carritos
+
+const carritosRouter = new Router()
+
+carritosRouter.post('/', async (req, res) =>{
+    res.json(await carritosAPi.guardar(req.body))
+})
+
+carritosRouter.delete('/:id', async (req, res) =>{
+    res.json(await carritosAPi.borrar(req.params.id))
+})
+
+carritosRouter.get('/:id?/productos', async (req, res) =>{
+    res.json(await carritosAPi.listar(req.params.id))
+});
+
+carritosRouter.post('/:id?/productos', async (req, res) =>{
+    res.json(await carritosAPi.guardarPorId(req.params.id))
+})
+
+
+carritosRouter.delete('/:id?/productos/:id_prod?', async (req, res) =>{
+    res.json(await carritosAPi.eliminarDelCarrito(req.params.id, req.params.id_prod))
+})
+
+
+//--------------------------------------------
+// configuro el servidor
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+app.use('/api/productos', productosRouter)
+app.use('/api/carritos', carritosRouter)
+
+module.exports = app
---
+++ Primera_Entrega/src/ContenedorArchivo.js
@@ -0,0 +1,143 @@
+const { promises: fs } = require("fs");
+
+class ContenedorArchivo {
+  constructor(ruta) {
+    this.ruta = ruta;
+  }
+
+  async listar(id) {
+    let rawdata = await fs.readFile(this.ruta);
+    let data = JSON.parse(rawdata);
+
+    let aux = data.filter((dat) => dat.id == id);
+
+    if (this.ruta.includes("Carritos")) {
+      aux = aux.map(function (e) {
+        return e.productos;
+      });
+    }
+
+    return aux;
+  }
+
+  async listarAll() {
+    let rawdata = await fs.readFile(this.ruta);
+    let data = JSON.parse(rawdata);
+
+    return data;
+  }
+
+  async guardarPorId(id) {
+    let rawdata = await fs.readFile("../Primera_Entrega/dbProductos.json");
+    let data = JSON.parse(rawdata);
+    let dataAux = [...data];
+
+    let datosCarro = await fs.readFile(this.ruta);
+    let dataNueva = JSON.parse(datosCarro);
+
+    dataAux = dataAux.map(function (obj) {
+      if (obj.id == id) {
+        dataNueva = dataNueva.map(function (e) {
+          e.productos.push(obj);
+          return e;
+        });
+      }
+    });
+
+    dataNueva = JSON.stringify(dataNueva);
+    await fs.writeFile(this.ruta, dataNueva);
+  }
+
+  async guardar(obj) {
+    let idNuevo = await this.getNextId();
+    obj.id = idNuevo;
+    obj.timestamp = Date.now();
+
+    if (this.ruta.includes("Carritos")) {
+      obj.productos = [];
+    }
+
+    let rawdata = await fs.readFile(this.ruta);
+    let data = JSON.parse(rawdata);
+    data.push(obj);
+    data = JSON.stringify(data);
+    await fs.writeFile(this.ruta, data);
+
+    return "Guardado con éxito";
+  }
+
+  async getNextId() {
+    let x = await this.listarAll();
+    let aux = [...x];
+    let id = 1;
+    if (aux) {
+      if (aux.length > 0) {
+        id = parseInt(aux[aux.length - 1].id) + 1;
+      }
+    }
+    return id;
+  }
+
+  async actualizar(elem, id) {
+    let rawdata = await fs.readFile(this.ruta);
+    let data = JSON.parse(rawdata);
+    let dataAux = [...data];
+
+    dataAux = dataAux.map(function (obj) {
+      if (obj.id == id) {
+        obj.nombre = elem.nombre;
+        obj.descripcion = elem.descripcion;
+        obj.codigo = elem.codigo;
+        obj.foto = elem.foto;
+        obj.precio = elem.precio;
+        obj.stock = elem.stock;
+        obj.timestamp = Date.now();
+      }
+      return obj;
+    });
+
+    dataAux = JSON.stringify(dataAux);
+    await fs.writeFile(this.ruta, dataAux);
+
+    return "Actualizado con éxito";
+  }
+
+  async borrar(id) {
+    let rawdata = await fs.readFile(this.ruta);
+    let data = JSON.parse(rawdata);
+    let dataAux = [...data];
+
+    dataAux = dataAux.filter(function (obj) {
+      if (obj.id != id) {
+        return obj;
+      }
+    });
+
+    dataAux = JSON.stringify(dataAux);
+    await fs.writeFile(this.ruta, dataAux);
+
+    return "Eliminado con éxito";
+  }
+
+  async eliminarDelCarrito(id, idProd) {
+    let rawdata = await fs.readFile(this.ruta);
+    let data = JSON.parse(rawdata);
+    let dataAux = [...data];
+
+    dataAux = dataAux.map(function (obj) {
+      if (obj.id == id) {
+        obj.productos = obj.productos.map(function (e) {
+           if (e.id != idProd) {
+            obj.productos = e;
+            return obj.productos;
+          }
+        });
+      }
+    });
+
+    // dataAux = JSON.stringify(dataAux);
+    // await fs.writeFile(this.ruta, dataAux);
+  }
+}
+
+module.exports = ContenedorArchivo;
