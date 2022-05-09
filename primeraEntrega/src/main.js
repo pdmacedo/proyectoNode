559ea5f81e2684205612b8b388a5fe513ffcacaa
Daniel Macedo <49345274+pdmacedo@users.noreply.github.com>
2022-05-09 16:56:55 +0100

    Delete primeraEntrega directory

--- primeraEntrega/src/main.js
+++
@@ -1,8 +0,0 @@
-// const app = require("./server.js");
-
-// const PORT = 8080;
-// const server = app.listen(PORT, () => {
-//     console.log(`Servidor en el puerto ${server.adress().port}`);
-// })
-
-// server.on('error', error => console.log(`Error en el servidor ${error}`));
